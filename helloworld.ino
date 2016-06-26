24efd1ad27b508a317b7b6528c2269f428bff5f3
Mtfujisan <darkyami_ryuu@yahoo.com>
2016-06-25 17:59:13 -0700

    Add files via upload

---
+++ helloworld.ino
@@ -0,0 +1,21 @@
+void setup() {
+  // put your setup code here, to run once:
+  
+  Serial.begin(9600);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  //Serial.println(analogRead(A0));
+  bargraph(analogRead(A0));
+  //delay(100);
+}
+
+void bargraph(int x) {
+  int i;
+  for (i = 0; i < x/10; i++){
+    Serial.print("*");
+   }
+  Serial.println();
+}
+
